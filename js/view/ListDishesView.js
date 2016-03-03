c34782387e78b012e36b22c4e4914aa11da74c73
daisydu <857635228@qq.com>
2016-03-03 19:38:50 +0100

    DataFromBigOvenAPI
    Successfully get the dish from BigOvenAPI

--- js/view/ListDishesView.js
+++ js/view/ListDishesView.js
@@ -1,65 +1,55 @@
 var ListDishesView = function (container, model){
 	// is a loop, print all dishes in that category
 	model.attach(this);
+	model.getRecipeJson();
     this.listAllDishes = container.find("#listAllDishes");
     this.selectType = container.find("#selection");
     this.search = container.find("#search");
     this.keyWords = container.find("#keyWords");
-    //console.log(this.selectType.length);
-    //var dishType = model.getSelectedDish('starter','eggs');
-	//var dishes = model.getAllDishes('starter');
-	var dishes = [];
-	dishes = model.getAllDishes('all');
-	//var dishes = model.getAllDishes(dishType);
+
+	// var dishes = [];
+	// dishes = model.getAllDishes('all');
 	var alldishesHtml = "";
-
-    for (var i = 0; i < dishes.length; i++) {
-
-		alldishesHtml +=  "<div class=\"col-xs-3 dishbox\">" + 
-		                       "<a href=\'#\' class=\'selectDish\' id=\'"+ dishes[i].id +"\'>" +
+	var dish = [];
+	
+	this.update = function(args){
+		if (args == "dish") {
+			 dish = model.dish;
+			 alldishesHtml +=  "<div class=\"col-xs-3 dishbox\">" + 
+		                       "<a href=\'#\' class=\'selectDish\' id=\'"+ dish.RecipeID +"\'>" +
 		                       "<div class=\" dish\" id=\'dishID\' > " +
-							      "<center>" + " <img src=\'images/" + dishes[i].image + "\' ></center> " +
-							      " <div class=\"dishname\">" + dishes[i].name + "</div>" +
+							      "<center>" + "<img src=\'" + dish.ImageURL + "\'></center> " +
+							      " <div class=\"dishname\">" + dish.Title + "</div>" +
 							   "</div></a>" + 
-							   "<div class=\"description\"> " + dishes[i].description + "</div>" +
+							   "<div class=\"description\"> " + dish.Description + "</div>" +
 					      "</div>";
-		// alldishesHtml +=  "<div class=\"col-sm-6 col-md-4\">" +
-		// 				    "<div class=\"thumbnail\" class=\'selectDish\' id=\'"+ dishes[i].id +"\'>" +
-		// 				      " <img src=\'images/" + dishes[i].image + "\' >" +
-		// 				       "<div class=\"caption\"> " +
-		// 				        "<h3>" +  dishes[i].name + "</h3>" +
-		// 				        "<p>" + dishes[i].description + "</p>" +
-		// 				     "</div>" +
-		// 				    "</div>" +
-		// 				  "</div>" ;
-					  }
+		};
 
-     this.listAllDishes.html(alldishesHtml);
+		this.listAllDishes.html(alldishesHtml);
+	}
 
-     this.update = function(args){
-        if (args == "dishType" || args == "filter") {
+ //     this.update = function(args){
+ //        if (args == "dishType" || args == "filter") {
         	
-        	var dishType = model.getSelectedDish();
-        	var filter = model.getFilter();
-        	var dishes = model.getAllDishes(dishType,filter);
-        	//console.log(filter);
+ //        	var dishType = model.getSelectedDish();
+ //        	var filter = model.getFilter();
+ //        	var dishes = model.getAllDishes(dishType,filter);
      
-        var alldishesHtml = "";
+ //        var alldishesHtml = "";
 
-    	for (var i = 0; i < dishes.length; i++) {
-    		//console.log("hi");
-			alldishesHtml +=  "<div class=\"col-xs-3 dishbox\">" + 
-		                       "<a href=\'#\' class=\'selectDish\' id=\'"+ dishes[i].id +"\'><div class=\" dish\" id=\'dishID\' > " +
-							     "<center>" + " <img src=\'images/" + dishes[i].image + "\' ></center> " +
-							     " <div class=\"dishname\">" + dishes[i].name + "</div>" +
-							   "</div></a>" + 
-							   "<div class=\"description\"> " + dishes[i].description + "</div>" +
-					      "</div>";
-		}
-        this.listAllDishes.html(alldishesHtml);
+ //    	for (var i = 0; i < dishes.length; i++) {
+	// 		alldishesHtml +=  "<div class=\"col-xs-3 dishbox\">" + 
+	// 	                       "<a href=\'#\' class=\'selectDish\' id=\'"+ dishes[i].id +"\'><div class=\" dish\" id=\'dishID\' > " +
+	// 						     "<center>" + " <img src=\'images/" + dishes[i].image + "\' ></center> " +
+	// 						     " <div class=\"dishname\">" + dishes[i].name + "</div>" +
+	// 						   "</div></a>" + 
+	// 						   "<div class=\"description\"> " + dishes[i].description + "</div>" +
+	// 				      "</div>";
+	// 	}
+ //        this.listAllDishes.html(alldishesHtml);
 
 
- 	};
- } 
+ // 	};
+ // } 
 }
 
